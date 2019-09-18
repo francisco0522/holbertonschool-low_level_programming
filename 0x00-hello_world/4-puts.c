511c6040d2cfa5e23a1592040a4fff9b7e814122
francisco0522 <pacho0522@gmail.com>
2019-09-18 18:23:45 +0000

    fix task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+*
+*
+*/
 #include <stdio.h>
 
 int main(void)
 {
-	char string[] = "\"Programming is like building a multilingual puzzle\n";
-	puts(string);
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
