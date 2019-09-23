d3cfbb75a5d1248aa473c4693f9e15b38464fbe1
francisco0522 <pacho0522@gmail.com>
2019-09-23 22:02:52 +0000

    Adding task 0-5

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,11 +1,20 @@
-#include<stdio.h>
+#include "holberton.h"
 /**
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-int main (void)
+int main(void)
 {
-	printf("Holberton\n");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+        _putchar('r');
+        _putchar('t');
+        _putchar('o');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
 }
