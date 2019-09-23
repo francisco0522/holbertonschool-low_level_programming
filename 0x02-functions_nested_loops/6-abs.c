d8e7759c36d2fdcb7fe8e7d09a25feb71194af6d
francisco0522 <pacho0522@gmail.com>
2019-09-23 23:02:15 +0000

    Adding task 7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _abs(int c)
+{
+
+	if (c < 0)
+	{
+	c = c * -1;
+	}
+	else
+	{
+	c = c;
+	}
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int c)
+{
+	int last;
+	last = c % 10;
+	if ( last < 0)
+	{
+	last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
+}
