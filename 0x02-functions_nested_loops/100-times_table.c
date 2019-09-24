9d6ca1d2856daea4f986843891a3ec978a8d3749
francisco0522 <pacho0522@gmail.com>
2019-09-24 23:20:29 +0000

    fix task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,13 +9,12 @@ void print_times_table(int n)
 	int x, y, z;
 
 	if (n > 15 || n < 0)
-	{
 	return;
-	}
 	else
 	{
 	for (x = 0; x <= n; x++)
 	{
+	_putchar ('0');
 	for (y = 0; y <= n; y++)
 	{
 	z = (x * y);
@@ -41,10 +40,6 @@ void print_times_table(int n)
 	_putchar (' ');
 	_putchar (' ');
 	_putchar (' ');
-	_putchar ((z % 10) + '0');
-	}
-	else
-	{
 	_putchar ((z % 10) + '0');
 	}
 	}
