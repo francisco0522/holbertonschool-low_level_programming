65a4ada7fe94ef04a678b6cda8cd7763b4e0a45e
francisco0522 <pacho0522@gmail.com>
2019-09-24 18:28:59 +0000

    fix task 2

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,9 +1,9 @@
-#ifdef HOLBERTON_H
+#ifndef HOLBERTON_H
 #define HOLBERTON_H
 
 #include <stdio.h>
 
-int positive_or_negative(int i);
+void positive_or_negative(int i);
 int largest_number(int, int, int);
 
 #endif /* HOLBERTON_H */
