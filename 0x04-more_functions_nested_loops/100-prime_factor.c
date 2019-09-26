9311a7ebc0af7c9eb8eda884d7dbc018d0a3c00d
francisco0522 <pacho0522@gmail.com>
2019-09-26 19:19:02 +0000

    Adding task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+* main - check the code for Holberton School students.
+*
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	long int num, i, maxf;
+
+	num = 612852475143;
+	i = 2;
+	while (i <= num)
+	{
+	if (num % i == 0)
+	{
+	num = num / i;
+	maxf = i;
+	}
+	else
+	{
+	i++;
+	}
+	}
+	printf("%ld ", maxf);
+	printf("\n");
+}
