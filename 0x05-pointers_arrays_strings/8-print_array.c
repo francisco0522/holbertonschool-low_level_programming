648a16b5f4f1c6aa695bee9f337fd6db0b9e9ee6
francisco0522 <pacho0522@gmail.com>
2019-10-01 19:46:37 +0000

    Adding task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* print_array - prints n elements of an array of integers
+* @a: int a
+* @n: int n
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	printf("%d", a[i]);
+	if (i < (n - 1))
+	{
+	printf(", ");
+	}
+	}
+	printf("\n");
+}
