475f28e2f85aab990910a158d3e84760042aea19
francisco0522 <pacho0522@gmail.com>
2019-10-01 14:58:00 +0000

    Adding task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include"holberton.h"
+/**
+* _puts - prints a string, followed by a new line, to stdout
+* @str: char str
+* Return: Always 0.
+*/
+void _puts(char *str)
+{
+	int i;
+	char c;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+	c =  str[i];
+	i++;
+	printf("%c", c);
+	}
+	printf("\n");
+}
