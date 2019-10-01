7c1598ea5ab8e8fc5e8fd1ba984aa9943d1ed75d
francisco0522 <pacho0522@gmail.com>
2019-10-01 20:07:55 +0000

    Adding task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* *_strcpy - copies the string pointed to by src to the buffer pointed to dest
+* @dest: char dest
+* @src: char src
+* Return: dest.
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	char a;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	a = src[i];
+	dest[i] = a;
+	}
+	return (dest);
+}
