26ef1673477fe20d603cb10c9f3506fe395ed025
francisco0522 <pacho0522@gmail.com>
2019-10-03 23:36:38 +0000

    Adding task 8

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,23 @@
+#include"holberton.h"
+/**
+* *rot13 -  encodes a string using rot13.
+* @a: char a
+* Return: Always 0.
+*/
+char *rot13(char *a)
+{
+	int i, j;
+	char b[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char c[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; b[j] != '\0'; j++) 
+		{
+			if (a[i] == b[j])
+			{
+			a[i] = c[j];
+			}
+		}
+	}
+	return (a);
+}
