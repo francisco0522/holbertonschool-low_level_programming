9d6020b89531b3d6d6e6da322f6260fc8e16f0ab
francisco0522 <pacho0522@gmail.com>
2019-10-03 23:39:02 +0000

    Adding task 8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -9,9 +9,10 @@ char *rot13(char *a)
 	int i, j;
 	char b[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char c[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	
 	for (i = 0; a[i] != '\0'; i++)
 	{
-		for (j = 0; b[j] != '\0'; j++) 
+		for (j = 0; b[j] != '\0'; j++)
 		{
 			if (a[i] == b[j])
 			{
