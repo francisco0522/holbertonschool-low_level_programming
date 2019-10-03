2e20dedf5bf2d3288d37da4a1143b2c29439a504
francisco0522 <pacho0522@gmail.com>
2019-10-03 23:11:21 +0000

    Adding task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,6 +1,6 @@
 #include"holberton.h"
 /**
-* *string_toupper - changes all lowercase letters of a string to uppercase
+* *leet -  encodes a string into 1337.
 * @a: char a
 * Return: Always 0.
 */
@@ -9,6 +9,7 @@ char *leet(char *a)
 	int i, j;
 	char b[] = "4307143071";
 	char c[] = "AEOTLaeotl";
+
 	for (i = 0; a[i] != '\0'; i++)
 	{
 		for (j = 0; c[j] != '\0'; j++)
