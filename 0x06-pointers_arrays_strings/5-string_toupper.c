2828416df661e28ed814b1402e92e2b6bdfb06de
francisco0522 <pacho0522@gmail.com>
2019-10-03 21:17:53 +0000

    Adding task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,15 +7,13 @@
 char *string_toupper(char *a)
 {
 	int i;
-	char b;
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
-	b = a[i];
-	if (a[i] > 96 && a[i] < 123)
-	{
-	a[i] = a[i] - 32;
-	}
+		if (a[i] > 96 && a[i] < 123)
+		{
+		[i] = a[i] - 32;
+		}
 	}
 	return (a);
 }
