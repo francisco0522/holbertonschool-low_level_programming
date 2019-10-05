228a86f2798ee91c71f5df4c1559886ea98c6da8
francisco0522 <pacho0522@gmail.com>
2019-10-05 16:51:52 +0000

    Adding task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include"holberton.h"
+#include<stdio.h>
+/**
+* main - prints the sum of all the multiples of 3 or 5 below 1024.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	long i, j, k, l, temp, d, m;
+
+	j = 0;
+	k = 1;
+	for (i = 0; i < 50; i++)
+	{
+	l = j + k;
+	temp = k;
+	k = l;
+	j = temp;
+	printf("%ld, ", l);
+	}
+	printf("\n");
+	return (0);
+}
