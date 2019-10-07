f6f6bbd145126ce2823c19531ece58e900562233
francisco0522 <pacho0522@gmail.com>
2019-10-07 18:24:11 +0000

    Adding task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* *_memset -  fills memory with a constant byte.
+* @s: char s
+* @b: char b
+* @n: int n
+*
+* Return: pointer to the memory area s.
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i, j;
+
+	j = n;
+	for (i = 0; i <= j; i++)
+	{
+	s[i] = b;
+	} 
+	return (s);
+}
