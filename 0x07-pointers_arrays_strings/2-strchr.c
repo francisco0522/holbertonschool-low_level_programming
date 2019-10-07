4c41e2c5b50702438ec7801eb0bae93dcb18060e
francisco0522 <pacho0522@gmail.com>
2019-10-07 19:06:22 +0000

    Fix task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,14 @@
 char *_strchr(char *s, char c)
 {
 	int i;
+	char *p;
 
 	for (i = 0; s[i] != '\0';)
 	{
 		if (s[i] == c)
 		{
-			return (s + i);
+			p = (s + i);
+			return (p);
 		}
 		else
 		{
