730781515f42f5b50a402fc35124f671cb86c0c4
francisco0522 <pacho0522@gmail.com>
2019-10-07 18:35:29 +0000

    Adding task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* *_memcpy - copies memory area
+* @dest: char dest
+* @src: char src
+* @n: unsigned int n
+*
+* Return: Nothing.
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i, j;
+
+	j = n;
+	for (i = 0; i < j; i++)
+	{
+	dest = src;
+	}
+	return (dest);
+}
