ba20f3d3e399209dd7613cdb5442bcd11ee23ac4
francisco0522 <pacho0522@gmail.com>
2019-10-09 19:06:04 +0000

    Adding task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,34 @@
+int sqrtHolb(int n, int a);
 #include "holberton.h"
-/*
+/**
 * _sqrt_recursion - returns the natural square root of a number
 * @n: int n
 * Return: Always 0.
 */
 int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (sqrtHolb(n, 0));
+	}
+}
+
+int sqrtHolb(int n, int a)
+{
+	if (a * a == n)
+	{
+		return (a);
+	}
+	else if (a * a > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (sqrtHolb(n, a + 1));
+	}
+}
