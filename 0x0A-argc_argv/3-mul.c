d111169d4ff08a48eabe293a8fd9883914894cf8
francisco0522 <pacho0522@gmail.com>
2019-10-12 20:29:55 +0000

    Adding task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,3 +7,19 @@
 */
 int main(int argc, char *argv[])
 {
+	int num1, num2, result;
+
+	if (argc < 3)
+	{
+	printf("Error");
+	return (1);
+	}
+	else
+	{
+	num1 = *argv[1] - '0';
+	num2 = *argv[2] - '0';
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
+	}
+}
