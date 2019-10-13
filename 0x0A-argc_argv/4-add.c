c6489d21f0324da1573fd6acf0db7e98eab73a66
francisco0522 <pacho0522@gmail.com>
2019-10-13 16:17:23 -0500

    Adding task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
 * main - returns (0)
 * @argc: int argc
@@ -7,3 +8,26 @@
 */
 int main(int argc, char *argv[])
 {
+int i, j, num, sum;
+
+sum = 0;
+if (argc < 2)
+{
+printf("0\n");
+}
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] < 48 || argv[i][j] > 57)
+{
+printf("Error\n");
+return (1);
+}
+}
+num = atoi(argv[i]);
+sum = sum + num;
+}
+printf("%d\n", sum);
+return (0);
+}
