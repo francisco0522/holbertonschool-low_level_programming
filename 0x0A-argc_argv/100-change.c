4bd783bb69a606a6f4bcb45e12b8d6d4150f6f86
francisco0522 <pacho0522@gmail.com>
2019-10-13 22:07:41 -0500

    fix task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,57 +8,39 @@
 */
 int main(int argc, char *argv[])
 {
-    int i, total, fijo, count;
-
-    count = 0;
-    if (argc != 2)
-    {
-    printf("Error\n");
-    return (1);
-    }
-    else
-    {
-    fijo = atoi(argv[1]);
-    total = atoi(argv[1]);
-    if (total < 0)
-    {
-    printf("0");
-    return (0);
-    }
-    for (i = 0; i < fijo;)
-    {
-    if (total % 25 == 0)
-    {
-    total = total - 25;
-    i = i + 25;
-    count++;
-    }
-    else if(total % 10 == 0)
-    {
-    total = total - 10;
-    i = i + 10;
-    count++;
-    }
-    else if (total % 5 == 0)
-    {
-    total = total - 5;
-    i = i + 5;
-    count++;
-    }
-    else if (total % 2 == 0)
-    {
-    total = total - 2;
-    i = i + 2;
-    count++;
-    }
-    else if (total % 1 == 0)
-    {
-    total = total - 1;
-    i = i + 1;
-    count++;
-    }
-    }
-    printf("%d\n", count);
-    return (0);
-    }
+int i, j,total, fijo, count;
+int dev[] = {25, 10, 5, 2, 1};
+    
+count = 0;
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+fijo = 13;
+total = 13;
+if (total < 0)
+{
+printf("0");
+return (0);
+}
+j = 0;
+for (i = 0; i < fijo;)
+{
+if (total - dev[j] >= 0)
+{
+total = total - dev[j];
+i = i + dev[j];
+count++;
+}
+else 
+{
+j++;
+}
+}   
+}
+printf("%d\n", count);
+return (0);
 }
