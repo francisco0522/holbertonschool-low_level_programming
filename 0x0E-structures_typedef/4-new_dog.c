304a527787f6d9a6ec9e3572e2c5319320aa1404
francisco0522 <pacho0522@gmail.com>
2019-10-21 23:52:36 +0000

    Adding task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - check the code for Holberton School students.
+ * @name: nombre
+ * @age: edad
+ * @owner: dueño
+ * Return: Always 0.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	int i, j, k, l;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+	for (i = 0; name[i] != '\0'; i++)
+		;
+	for (j = 0; owner[j] != '\0'; j++)
+		;
+	d->name = malloc(sizeof(char) * i);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	for (k = 0; k < i; k++)
+		d->name[k] = name[k];
+	d->age = age;
+	d->owner = malloc(sizeof(char) * j);
+	if (d->owner == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	for (l = 0; l < j; l++)
+		d->owner[l] = owner[l];
+	return (d);
+}
