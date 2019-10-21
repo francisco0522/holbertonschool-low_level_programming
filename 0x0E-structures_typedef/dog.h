7d10a96e2d787815095cfb22d07285e95f138e8e
francisco0522 <pacho0522@gmail.com>
2019-10-21 22:21:28 +0000

    Adding task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+* struct dog - Short description
+* @char: First member
+* @age: Second member
+* @owner: Third member
+* Description: Longer description
+*/
 struct dog
 {
 	char *name;
