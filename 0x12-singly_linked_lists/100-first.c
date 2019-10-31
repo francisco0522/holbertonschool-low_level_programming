a258ba0d66600a10f9e60e2ec0b802a77f2b1da2
francisco0522 <pacho0522@gmail.com>
2019-10-31 22:40:13 +0000

    Adding task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * before - printf before main.
+ *
+ * Return: Always 0.
+ */
+void before(void) __attribute__ ((constructor));
+
+void before(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
