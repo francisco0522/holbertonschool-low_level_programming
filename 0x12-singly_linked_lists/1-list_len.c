5f10397e75893b2f277ab4641f4a6b2b1aaece4d
francisco0522 <pacho0522@gmail.com>
2019-10-31 17:07:44 +0000

    Fix task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -6,12 +6,12 @@
  */
 size_t list_len(const list_t *h)
 {
-        int i = 0;
+	int i = 0;
 
-        while (h != NULL)
-        {
-                h = h->next;
-                i++;
-        }
-        return (i);
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
