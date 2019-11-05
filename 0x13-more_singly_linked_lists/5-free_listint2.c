2fd5fb33076faa63867f02e1e0f81f068e951692
francisco0522 <pacho0522@gmail.com>
2019-11-05 16:28:48 +0000

    Fix task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * free_listint2 - frees a listint_t list
  * @head: listint_t
@@ -9,7 +8,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *copy;
 
-	if (*head != NULL)
+	if (head != NULL)
 	{
 		while ((*head)->next != NULL)
 		{
