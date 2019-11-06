d6045355049151c80b7fbd7512433519c2f2c9e5
francisco0522 <pacho0522@gmail.com>
2019-11-06 00:25:24 +0000

    fix task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	while (i < (idx - 1))
 	{
-		if (*head == NULL)
+		if (node == NULL)
 		{
 			free(copy);
 			return (NULL);
