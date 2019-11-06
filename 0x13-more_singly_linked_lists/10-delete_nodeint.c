aea03f215db748a34425a1039fd8bbecfebe1218
francisco0522 <pacho0522@gmail.com>
2019-11-06 01:04:47 +0000

    Adding task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 /**
- * insert_nodeint_at_index - inserts a new node at a given position.
+ * delete_nodeint_at_index - inserts a new node at a given position.
  * @head: listint_t
  * @idx: int
  * @n: n
  * Return: sum.
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
 	listint_t *copy, *node;
@@ -15,27 +15,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	node = *head;
 	copy = malloc(sizeof(listint_t));
 	if (copy == NULL)
-		return (NULL);
 
-	copy->n = n;
-	if (idx == 0)
+	if (index == 0)
 	{
 		copy->next = node;
 		*head = copy;
-		return (copy);
 	}
 
-	while (i < (idx - 1))
+	while (i < (index - 1))
 	{
 		if (node == NULL)
 		{
 			free(copy);
-			return (NULL);
 		}
 		node = node->next;
 		i++;
 	}
 		copy->next = node->next;
 		node->next = copy;
-		return (copy);
+	return (0);
 }
