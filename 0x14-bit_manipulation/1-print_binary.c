bf5844ff075748fb4e75722d196f7ccbe99c05fb
francisco0522 <pacho0522@gmail.com>
2019-11-08 15:54:10 +0000

    Adding task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,16 +1,15 @@
 #include "holberton.h"
 /**
- * get_bit - check the code for Holberton School students.
+ * print_binary - prints the binary representation of a number.
  * @n: int;
- * @index: int;
  * Return: the converted number, or 0
  */
 void print_binary(unsigned long int n)
 {
-        if (n < 2)
+	if (n < 2)
 	{
-	_putchar(n + '0');
-	return;
+		_putchar(n + '0');
+		return;
 	}
 	print_binary(n >> 1);
 	_putchar((n & 1) + '0');
