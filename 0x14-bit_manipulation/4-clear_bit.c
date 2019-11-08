69ddafd34d8c091ac647a2a958f93eeecaa6e899
francisco0522 <pacho0522@gmail.com>
2019-11-08 00:57:03 +0000

    task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,10 +9,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > sizeof(n) * 8)
 		return (-1);
-	else if (*n != 0)
-	{
-		*n = *n - (1 << index);
-		return (1);
-	}
-	return (0);
+	*n = *n - (1 << index);
+	return (1);
 }
