cced393a91619335c900e0e59cd66cc929246ce0
francisco0522 <pacho0522@gmail.com>
2019-11-08 00:26:25 +0000

    Adding task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * get_bit - check the code for Holberton School students.
+ * @b: char;
+ * Return: the converted number, or 0
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+		return (-1);
+	return (n >> index & 1);
+
+}
+~
