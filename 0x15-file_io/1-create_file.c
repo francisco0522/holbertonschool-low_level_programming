d733d6636a1cef4cc96145fe68b7dced92f62595
francisco0522 <pacho0522@gmail.com>
2019-11-12 21:20:41 +0000

    Fixing task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	{
 	}
 
-	op = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (text_content == NULL)
 		return (1);
 	if (op == -1)
