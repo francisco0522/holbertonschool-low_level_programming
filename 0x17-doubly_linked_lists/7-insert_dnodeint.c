3f4bd1257e4284cdf17839a07953bbb14b183395
PachoHouse <pacho0522@gmail.com>
2019-12-15 17:07:12 +0000

    Fix task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	copy->n = n;
 	while (node != NULL && node->prev != NULL)
 	{
-		headcopy = headcopy->prev;
+		node = node->prev;
 		*h = (*h)->prev;
 	}
 	if (idx == 0)
