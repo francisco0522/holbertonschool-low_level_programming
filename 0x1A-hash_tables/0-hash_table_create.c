7e72d3eeb57ebcd4eafd0a972826c4a0c7277fd4
root <root@DESKTOP-GAH54SL.localdomain>
2020-01-18 15:35:59 -0500

    Adding task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,10 +1,32 @@
 #include "hash_tables.h"
 /**
-* main - returns (0)
-* @argc: int argc
-* @argv: char argv
-* Return: Always 0.
-*/
+ * hash_table_create - returns (hash)
+ * @size: int size
+ * Return: hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
+	hash_table_t *hash = malloc(sizeof(hash_table_t) * 1);
+
+	if (!hash)
+	{
+		free(hash);
+		return (NULL);
+	}
+
+	hash->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash->array == NULL)
+	{
+		free(hash);
+		return (NULL);
+	}
+
+	for (i = 0; i < size ; i++)
+	{
+		hash->array[i] = NULL;
+	}
+	hash->size = size;
+
+	return (hash);
 }
