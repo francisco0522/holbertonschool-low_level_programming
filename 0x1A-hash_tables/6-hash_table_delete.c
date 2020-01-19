2efbb58b870b13afdd84e369a76e303e8cfb758a
root <root@DESKTOP-GAH54SL.localdomain>
2020-01-19 15:30:40 -0500

    Fixing task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -12,7 +12,7 @@ void hash_table_delete(hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		first = ht->array[i];
-		while((sec = first))
+		while ((sec = first))
 		{
 			first = first->next;
 			free(sec->key);
