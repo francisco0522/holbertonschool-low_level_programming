dc75b89975112d947022fd994f3aea9695b20da0
francisco0522 <pacho0522@gmail.com>
2020-02-21 19:55:29 -0500

    Fixing task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """island_perimeter"""
 
+
 def island_perimeter(grid):
     par = 0
     numrows = len(grid)
@@ -15,9 +16,9 @@ def island_perimeter(grid):
                 if x != 0 and grid[y][x - 1] != 1:
                     par += 1
                 if x != numcols and grid[y][x + 1] != 1:
-                    par +=1
+                    par += 1
                 if y != 0 and grid[y - 1][x] != 1:
-                    par +=1
+                    par += 1
                 if y != numrows and grid[y + 1][x] != 1:
-                    par +=1
+                    par += 1
     return (par)
